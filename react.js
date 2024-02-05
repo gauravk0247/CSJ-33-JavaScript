3a57249540e017f4c6a5763e923b9d41edddeb2d
gauravk0247 <gauravkhairnar2106@gmail.com>
2024-02-05 23:06:07 +0530

    JS

---
+++ react.js
@@ -0,0 +1,10 @@
+import react from 'react';
+import {View, Text} from 'react-native';
+const App = ()=>{
+    return(
+        <View>
+            <Text style={{fontSize: 30}}>Hii React Native</Text>
+        </View>
+    )
+}
+export default App;
