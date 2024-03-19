c710686d5c7e383c24c4d044248f827f0b9f28c9
gauravk0247 <gauravkhairnar2106@gmail.com>
2024-03-19 22:00:20 +0530

    JS

--- level.js
+++ level.js
@@ -1,8 +1,19 @@
-function myFact(n){
-    if(n == 0 || n == 1){
-        return n;
-    }else{
-        return n * myFact(n-1);
-    }
+// function myFact(n){
+//     if(n == 0 || n == 1){
+//         return n;
+//     }else{
+//         return n * myFact(n-1);
+//     }
+// }
+// console.log(myFact(5))
+
+
+function inner(a,b, callback){
+    let result =  a + b;
+    callback(result)
 }
-console.log(myFact(5))
+function outer(result){
+    console.log(result)
+}
+inner(4, 5, outer)
+// console.log(a)
