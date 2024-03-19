fce18557fde713f6e56563249bfaff9019f237c1
gauravk0247 <gauravkhairnar2106@gmail.com>
2024-03-19 22:04:23 +0530

    JS

--- level.js
+++ level.js
@@ -17,3 +17,23 @@ function outer(result){
 }
 inner(4, 5, outer)
 // console.log(a)
+
+function lexi(){
+    let a = 45;
+    function lexa1(){
+        console.log(a)
+    }
+    lexa1()
+}
+lexi()
+
+let p = new Promise(()=>{
+    setInterval((resolve, reject)=>{
+            resolve(89);
+    }, 3000)
+})
+.then(()=>{
+    console.log("Success")
+}).catch(()=>{
+    console.log("Failed")
+})
